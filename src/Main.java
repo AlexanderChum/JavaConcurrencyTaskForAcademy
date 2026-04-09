1098d3ecd4635d6ad4df665537ca33abef369cf5
AlexanderChum <Senneiko@yandex.ru>
2026-04-09 03:12:12 +0300

    feat: custom realisation of BlockingQueue

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        BlockingQueue queue = new BlockingQueue(5);
 
+        new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                try {
+                    queue.enqueue(i);
+                    System.out.println("Напиши какую цифру ты сохранил " + i);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10 ; i++) {
+                try {
+                    Object o = queue.dequeue();
+                    System.out.println("Напиши какую цифру ты достал " + o);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
     }
 }
